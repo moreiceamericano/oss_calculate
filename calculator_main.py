e155a39a6d7d47ffc736103f53f9a8c8d55df548
jeongwon <144416517+moreiceamericano@users.noreply.github.com>
2023-12-21 20:48:47 +0900

    calculator_main.py 코드 복원, 재시작

--- calculator_main.py
+++ calculator_main.py
@@ -7,22 +7,25 @@ class Main(QDialog):
         self.init_ui()
 
     def init_ui(self):
-        main_layout = QGridLayout()
+        main_layout = QVBoxLayout()
 
         ### 각 위젯을 배치할 레이아웃을 미리 만들어 둠
-        layout_operation = QGridLayout()
-        layout_clear_equal = QGridLayout()
+        layout_operation = QHBoxLayout()
+        layout_clear_equal = QHBoxLayout()
         layout_number = QGridLayout()
-        layout_equation_solution = QGridLayout()
+        layout_equation_solution = QFormLayout()
 
-        # 수식 입력, 답출력 칸을 하나로 합치기
         ### 수식 입력과 답 출력을 위한 LineEdit 위젯 생성
-        self.equ_sol = QLineEdit("")
+        label_equation = QLabel("Equation: ")
+        label_solution = QLabel("Solution: ")
+        self.equation = QLineEdit("")
+        self.solution = QLineEdit("")
 
         ### layout_equation_solution 레이아웃에 수식, 답 위젯을 추가
-        layout_equation_solution.addRow(self.equ_sol)
+        layout_equation_solution.addRow(label_equation, self.equation)
+        layout_equation_solution.addRow(label_solution, self.solution)
 
-        ### 사칙연산 버튼 생성
+        ### 사칙연산산 버튼 생성
         button_plus = QPushButton("+")
         button_minus = QPushButton("-")
         button_product = QPushButton("x")
